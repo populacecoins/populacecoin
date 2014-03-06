832cd161ffbde01e7def8e43e0a720a17fd4ba8e
dotcoin <dotcoin@live.com>
2014-03-06 11:43:09 +0800

    First commit

---
+++ src/irc.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Carbonemissiontradecoin developers
+// Copyright (c) 2013-2079 CT DEV
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef CARBONEMISSIONTRADECOIN_IRC_H
+#define CARBONEMISSIONTRADECOIN_IRC_H
+
+void ThreadIRCSeed();
+
+extern int nGotIRCAddresses;
+
+#endif
